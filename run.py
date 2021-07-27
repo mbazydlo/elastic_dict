48d46d8cdf6c448399f60602562c23cca85fad73
Marcin Bazydlo <marcin.p.bazydlo@gmail.com>
2021-07-27 23:50:11 +0200

    v0.1.1 - Added get_value_from_string_keys function

--- run.py
+++ run.py
@@ -9,3 +9,12 @@ class ElasticDict:
     def _parse_string_keys(self, keys_as_string, delimiter=','):
         list_of_keys = keys_as_string.split(delimiter)
         return list_of_keys
+    
+    def get_value_from_string_keys(self, keys_as_string, delimiter=','):
+        list_of_keys = self._parse_string_keys(keys_as_string, delimiter=',')
+        temporary_dict = self.source_dict
+        
+        for key in list_of_keys:
+            temporary_dict = temporary_dict.get(key, dict())
+        
+        return temporary_dict
--- tests/test_elasticdict.py
+++ tests/test_elasticdict.py
@@ -13,6 +13,15 @@ def prepare_dictionary(request):
     
     return test_dictionary
 
+@pytest.fixture()
+def prepare_nesteted_dictionary():
+    location = os.path.join(os.getcwd(), 'tests', 'fixtures', 'dictionary_3.json')
+
+    with open(location, encoding='utf-8') as file:
+        test_dictionary = json.load(file)
+    
+    return test_dictionary
+
 
 def test_create_instance_elasticdict(prepare_dictionary):
     obj = ElasticDict(prepare_dictionary)
@@ -22,3 +31,8 @@ def test_create_instance_elasticdict(prepare_dictionary):
 def test_parse_string_keys(prepare_dictionary):
     result = ElasticDict(prepare_dictionary)._parse_string_keys("key1,key2,key3")
     assert result == ["key1", "key2", "key3"]
+
+
+def test_get_keys_from_string_keys(prepare_nesteted_dictionary):
+    assert prepare_nesteted_dictionary.get("address", {}).get("number", {}) == 1
+    assert prepare_nesteted_dictionary.get("address", {}).get("nonExisting", {}) == {}
