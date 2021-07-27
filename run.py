980f04ef6bdb3e7416d99760f6d81dd9cea01512
Marcin Bazydlo <marcin.p.bazydlo@gmail.com>
2021-07-27 23:25:53 +0200

    v0.1.0 - Added string keys splitter

--- run.py
+++ run.py
@@ -6,3 +6,6 @@ class ElasticDict:
     def __init__(self, source_dict):
         self.source_dict = source_dict
 
+    def _parse_string_keys(self, keys_as_string, delimiter=','):
+        list_of_keys = keys_as_string.split(delimiter)
+        return list_of_keys
--- tests/test_elasticdict.py
+++ tests/test_elasticdict.py
@@ -1,7 +1,24 @@
+import os
+import json
 import pytest
 
 from ..run import ElasticDict
 
-def test_create_instance_elasticdict():
-    obj = ElasticDict({})
+@pytest.fixture(params=[f'dictionary_{number}.json' for number in range(1, 4)])
+def prepare_dictionary(request):
+    location = os.path.join(os.getcwd(), 'tests', 'fixtures', request.param)
+
+    with open(location, encoding='utf-8') as file:
+        test_dictionary = json.load(file)
+    
+    return test_dictionary
+
+
+def test_create_instance_elasticdict(prepare_dictionary):
+    obj = ElasticDict(prepare_dictionary)
     assert obj
+
+
+def test_parse_string_keys(prepare_dictionary):
+    result = ElasticDict(prepare_dictionary)._parse_string_keys("key1,key2,key3")
+    assert result == ["key1", "key2", "key3"]
