74041a4206c5347afa4fec8d9fccac71e2edd072
Marcin Bazydło <marcin.p.bazydlo@gmail.com>
2022-01-06 15:09:56 +0100

    v0.2.5 - Added flatted_keys function + typing for elastic.py

--- elasticdict/elastic.py
+++ elasticdict/elastic.py
@@ -1,7 +1,10 @@
 """
 Ready for documentation.
 """
-from collections.abc import MutableMapping
+from collections.abc import MutableMapping, KeysView, ValuesView, ItemsView
+from typing import Dict, Any, List
+
+from elasticdict.errors import DuplicatedKeyError
 
 
 class ElasticDict(MutableMapping):
@@ -63,12 +66,12 @@ class ElasticDict(MutableMapping):
     def __repr__(self):
         return str(self.source_dict)
     
-    def _check_input_type(self, value):
+    def _check_input_type(self, value) -> bool:
         if value.__class__ not in (dict,):
             raise TypeError('Input is not a dict')
         return True
 
-    def get_all(self, key: str, only: str = None, exclude: str = None):
+    def get_all(self, key: str, only: str = None, exclude: str = None) -> List[Any]:
         """
         Generates all values that match to the passed key.
 
@@ -93,7 +96,7 @@ class ElasticDict(MutableMapping):
 
         return found_keys
     
-    def get_one(self, key):
+    def get_one(self, key: str) -> Any:
         """
         Returns first key of matches.
 
@@ -103,11 +106,11 @@ class ElasticDict(MutableMapping):
         """
         return result[0] if (result := self.get_all(key)) else None
 
-    def _parse_string_keys(self, keys_as_string):
+    def _parse_string_keys(self, keys_as_string: str) -> List[str]:
         list_of_keys = keys_as_string.split(self.delimiter)
         return list_of_keys
     
-    def get_value_from_string_keys(self, keys_as_string, value=None):
+    def get_value_from_string_keys(self, keys_as_string: str, value=None):
         *keys, last_key = self._parse_string_keys(keys_as_string)
         temporary_dict = self.source_dict
         
@@ -117,7 +120,7 @@ class ElasticDict(MutableMapping):
         temporary_dict = temporary_dict.get(last_key, value)
         return temporary_dict
     
-    def create_step_dict(self, value=None):
+    def create_step_dict(self, value: Dict[str, Any] = None):
         if not value:
             value = self.source_dict
 
@@ -130,21 +133,40 @@ class ElasticDict(MutableMapping):
                 self.step_dict['.'.join(self.parts)] = value
             self.parts.pop()
 
-    def keys(self, source: str = 'source_dict'):
+    def flatted_keys(self, unique_only: bool = False) -> Dict[str, Any]:
+        flatted_dict = dict()
+        non_unique_keys = set()
+
+        for key, value in self.step_dict.items():
+            *_, flatted_key = key.split(self.delimiter)
+
+            if unique_only and (flatted_key in flatted_dict):
+                non_unique_keys.add(flatted_key)
+                continue
+
+            flatted_dict[flatted_key] = value
+
+        if non_unique_keys:
+            raise DuplicatedKeyError(
+                f'Dictionary cannot be flatted as there are non unique keys: {non_unique_keys}'
+            )
+        return flatted_dict
+
+    def keys(self, source: str = 'source_dict') -> KeysView:
         return self.__getattribute__(source).keys()
 
-    def values(self, source: str = 'source_dict'):
+    def values(self, source: str = 'source_dict') -> ValuesView:
         return self.__getattribute__(source).values()
 
-    def items(self, source: str = 'source_dict'):
+    def items(self, source: str = 'source_dict') -> ItemsView:
         return self.__getattribute__(source).items()
 
-    def elastic_keys(self):
+    def elastic_keys(self) -> KeysView:
         return self.keys('step_dict')
 
-    def elastic_values(self):
+    def elastic_values(self) -> ValuesView:
         return self.values('step_dict')
 
-    def elastic_items(self):
+    def elastic_items(self) -> ItemsView:
         return self.items('step_dict')
 
